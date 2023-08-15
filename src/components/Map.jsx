4fa5ed7749bad21098090ce1d68079fea420bab2
Mertcan Nerse <mertcannerse@outlook.com>
2023-08-15 13:07:14 +0300

    tailwind style updated

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -166,44 +166,32 @@ const GoogleMap = () => {
               </button>
             </div>
           </div>
-          <div
-            style={{
-              borderLeft: "1px solid rgba(0,0,0,0.1)",
-              marginBottom: "40px",
-            }}
-          >
-            <div style={{ display: "grid", gridTemplateColumns: "auto 1fr" }}>
+          <div className="border-l-[1px] mb-20">
+            <div className="grid grid-cols-[auto_1fr]">
               <div>
                 <img src={icon1} alt="icon1" />
               </div>
               <div>
-                <h2 style={{ color: "#092D9B" }}>Farktor Merkez Ofis</h2>
-                <p style={{ color: "#181A46", opacity: "0.6" }}>
+                <h2 className="text-[#092D9B]">Farktor Merkez Ofis</h2>
+                <p className="text-[181A46] opacity-[0.6]">
                   Ulugazi mah. 19 mayıs blv. no:33 kat:1 D:2 İlkadım/Samsun
                 </p>
               </div>
             </div>
-            <div style={{ display: "grid", gridTemplateColumns: "auto 1fr" }}>
+            <div className="grid grid-cols-[auto_1fr]">
               <div>
                 <img src={icon2} alt="icon2" />
               </div>
               <div>
-                <p style={{ color: "#181A46" }}>Çağrı merkezi: 0850 259 3333</p>
+                <p>Çağrı merkezi: 0850 259 3333</p>
                 <p style={{ color: "#181A46" }}>
                   (9.30 - 18.30 arasında ulaşabilirsiniz)
                 </p>
               </div>
             </div>
-            <div style={{ margin: "0 80px" }} onClick={changeAdress1}>
+            <div className="m-[0_40px]" onClick={changeAdress1}>
               <button
-                style={{
-                  background: "transparent",
-                  borderRadius: "20px",
-                  height: "40px",
-                  width: "150px",
-                  color: "#092D9B",
-                  border: "1px solid blue",
-                }}
+                className="bg-transparent	rounded-2xl h-10 w-40 text-[#092D9B] border-[1px] border-[#092D9B]"
                 onClick={() => handleMarkerClick(markers[1])}
               >
                 Haritada Göster
@@ -211,7 +199,7 @@ const GoogleMap = () => {
             </div>
           </div>
         </div>
-        <div id="map" style={{ width: "886px", height: "400px" }}></div>
+        <div className="w-[886px] h-[585px]" id="map"></div>
       </div>
     </div>
   );
